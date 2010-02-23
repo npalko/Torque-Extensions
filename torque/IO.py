4473d117fed5f814d269ccb26f8def57481e0e7b
Nicholas Palko <npalko@tepper.cmu.edu>
2010-02-22 19:50:55 -0500

    added torque namespace

---
+++ torque/IO.py
@@ -0,0 +1,11 @@
+		
+	
+def loadFromXML(file)
+	pass
+def writeToXML(database, file=database.name)
+	pass	
+	
+def loadFromPickle(file)
+	pass
+def writeToPickle(database, file=database.name)
+	pass
