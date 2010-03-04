42737688be0b6b0cb546d9e4c13f97a6c33f4bf1
Nicholas Palko <npalko@tepper.cmu.edu>
2010-03-04 15:36:38 -0500

    added mysql import

--- torque/schemaio.py
+++ torque/schemaio.py
@@ -1,27 +1,94 @@
 from __future__ import print_function
 
 import cPickle as pickle
+import torque.model
+
 from torque.model import SQLType, KeyAction
 
 try:
     from lxml import etree
 except ImportError:
     print('Couldn''t load lxml. XML IO not available')
+    
+try:
+    import sqlalchemy
+except ImportError:
+    print('Couldn''t load sqlalchemy. SQLAlchemy ORM not available')
+    
 
 	
 class MySQLAdaptor(object):
-	def __init__(self):
-		pass
-	def toModel(self, physicalModel):
-		"""
-			physicalModel = grt.root.wb.doc.physicalModels[0]
-			database = torque.mysqlwb.adaptor(physicalModel)
-		
-		"""
-		pass
+    def __init__(self):
+        """ """
+
+        self._TextToTypeMap = {'INT': SQLType.INT,
+            }
+        
+        self._TypesWithLength = [SQLType.CHAR, SQLType.NCHAR, SQLType.NVARCHAR]
+    def getWorkingDir(self, grtRoot):
+        """return the working directory of the model, so that we know where
+        to save the additional generated files"""
+        pass
+    def toModel(self, physicalModel):
+        """Usage:
+
+        physicalModel = grt.root.wb.doc.physicalModels[0]
+        database = torque.mysqlwb.adaptor(physicalModel)
+        """
+        
+        schemata = physicalModel.catalog.schemata[0]
+        dModel = torque.model.Database(name=schemata.name)
+        
+        for t in schemata.tables:
+            tModel = torque.model.Table(t.name, comment=t.comment)
+            
+            # TODO: only works for one column as primary key
+            primaryKeyName = t.primaryKey.columns[0].referencedColumn.name
+            
+            for c in t.columns:
+                cModel = torque.model.Column(c.name, comment=c.comment)
+                
+                cModel.autoIncrement = c.autoIncrement
+                cModel.primaryKey = (c.name is primaryKeyName)
+                cModel.default = c.defaultValue
+                cModel.defaultIsNull = c.defaultValueIsNull
+                cModel.nullable = not c.isNotNull
+                cModel.datatype = self._TextToTypeMap[c.formattedType]
+                
+                if cModel.datatype in self._TypesWithLength:
+                    cModel.length = c.length
+                if cModel.datatype is SQLType.DECIMAL:
+                    cModel.precision = c.precision
+                    cModel.scale = c.scale
+                
+                tModel.column.append(cModel)
+            
+            for k in t.foreignKeys:
+                kModel = torque.model.ForeignKey(k.name)
+                
+                k.referencedTable.name
+                
+                for kc in k.columns:
+                    # create reference to column object already existing in
+                    # the model data structure rather than creating new 
+                    # objects
+                    kc.name
+                    
+                tModel.foreignKey.append(kModel)
+                    
+            for i in t.indicies:
+                iModel = torque.model.Index(i.name)
+                
+                tModel.index.append(iModel)
+                
+            
+            dModel.table.append(tModel)
+            
+        return dModel
 
 class XML(object):
-    def __init__(self, dtd='torque-extensions.dtd'):
+    def __init__(self, dtd='http://github.com/npalko/Torque-Extensions' \
+                 '/raw/master/torque-extensions.dtd'):
     
         # create bi-directional mapping between types in torque.model and text
         # expected in XML file
@@ -71,16 +138,22 @@ class XML(object):
                 a['type'] = self._TypeToTextMap[column.datatype]
                 if column.description is not None:
                     a['description'] = column.description
-                if column.primaryKey is not None:	
+                if column.default is not None:
+                    a['default'] = column.default
+                if column.length is not None:
+                    a['length'] = column.length
+                if column.precision is not None:
+                    a['precision'] = column.precision
+                if column.scale is not None:
+                    a['scale'] = column.scale
+                if column.primaryKey:	
                     a['primaryKey'] = self._BoolToTextMap[column.primaryKey] 
-                if column.autoIncrement is not None:
+                if column.autoIncrement:
                     a['autoIncrement'] = self._BoolToTextMap[column.autoIncrement]
-                    
-                ##required='',
-                ##precision='',
-                ##scale='',
-                ##length='',
-                ##default='',
+                if column.requried:
+                    a['required'] = self._BoolToTextMap[column.requried]
+                if column.defaultIsNull:
+                    a['defaultIsNull'] = self._BoolToTextMap[column.defaultIsNull]
                 
             for index in table.getIndex():
                 xmlIndex = etree.SubElement(xmlTable, 'index')
@@ -137,8 +210,25 @@ class LiteSQL(object):
         pass
         
 
-class SQLAlchemy(object):
-    def write(self, database, file=None):
+class SQLAlchemyAdaptor(object):
+    """
+        lib/sqlalchemy/schema.py
+        class MetaData(SchemaItem)
+        
+        line 1698
+        
+        use reflection interface MetaData.reflect() as a way of populating
+        MetaData from torque.model
+    
+    """
+    def __init__(self):
+        pass
+    def toMetaData(self, database):
+        """return sqlalchemy.metadata for the given torque.model.Database"""
+        pass
+    def toModel(self, metaData):
+        """return torque.model.Database for the given sqlalchemy.metadata"""
         pass
         
-	
+
+        
