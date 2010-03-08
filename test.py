a8deb4458a2c4b1b7956ad917a0e1fe415475d85
Nicholas Palko <npalko@tepper.cmu.edu>
2010-03-08 12:08:38 -0500

    streamlined object creation for testing

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import sys
 sys.path.append('C:\Users\Nicholas Palko\Documents\Development\Torque-Extensions')
-import torque.adaptor
+import torque
 
-physicalModel = grt.root.wb.doc.physicalModels[0]
+schemata = grt.root.wb.doc.physicalModels[0].catalog.schemata[0]
 
-reload(torque.model)
-reload(torque.adaptor)
+reload(torque)
 wbAdaptor = torque.adaptor.MySQLWorkbench()
-database = wbAdaptor.toModel(schemata)
+db = wbAdaptor.toModel(schemata)
 
 xw = torque.adaptor.XML()
-xw.write(database)
+xw.write(db)
 
